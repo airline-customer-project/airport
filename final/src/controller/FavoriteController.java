f0c570ec7fd80087ba91b411513cc93da13645b9
h-sw <hansi100@naver.com>
2022-12-10 20:52:52 +0900

    fix: 즐겨찾기 값 이슈 해결

--- final/src/controller/FavoriteController.java
+++ final/src/controller/FavoriteController.java
@@ -3,7 +3,7 @@ package controller;
 public class FavoriteController {
 	static public String GET_ALL = "SELECT * FROM favorite LEFT JOIN airport ON airport.id = airport_id";
 	static public String WRITE_ONE(String id) {
-		return "INSERT INTO favorite (airport_id) VALUES ( " + id + " )";
+		return "INSERT INTO favorite (id, airport_id) VALUES ( " + id + ", " + id + " )";
 	}
 	static public String GET_ONE(String id) {
 		return "SELECT airport.* FROM favorite LEFT JOIN airport ON airport.id = airport_id WHERE airport_id = " + id;
